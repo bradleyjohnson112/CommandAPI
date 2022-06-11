c90fb724674197bd0d76dd1de625da3cb1c53984
bradley <bradleyworkacc@outlook.com>
2022-06-11 03:42:57 +0100

    Added all controller actions"

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
+using AutoMapper;
+using Newtonsoft.Json.Serialization;
 
 namespace CommandAPI
 {
@@ -31,7 +33,12 @@ namespace CommandAPI
 
       services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
 
-      services.AddControllers();
+      services.AddControllers().AddNewtonsoftJson(s =>
+      {
+        s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+      });
+
+      services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
       services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
     }
